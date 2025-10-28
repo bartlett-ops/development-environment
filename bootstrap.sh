a8263fcbcbd61bab236b0c0b14304d2dcdf14689
Tom Bartlett <thomas.bartlett.1992@gmail.com>
2025-10-28 18:07:06 +0000

    Add basic skeleton

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+IFS='\n\t'
+
+if ! command -v ansible-playbook >/dev/null; then
+    sudo dnf install -y git python3 python3-pip
+    pip install ansible
+fi
+
+if [[ $(basename "${PWD}") != "development-environment" ]]; then
+    echo "Cloning project"
+    git clone https://github.com/bartlett-ops/development-environment.git
+    cd development-environment
+fi
+
+ansible-playbook -i ./hosts.yaml --ask-become-pass ./site.yaml
